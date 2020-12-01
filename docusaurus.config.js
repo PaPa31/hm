127570ed586e31af5e0e2dbf63e60907d2b85662
papa <visidoor@yandex.ru>
2020-12-01 06:44:49 +0500

    fix(config): change two editUrls
    to make editUrl links work
    
    	modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,13 +88,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/papa31/hm/edit/master/website/',
+            'https://github.com/papa31/hm/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/papa31/hm/edit/master/website/blog/',
+            'https://github.com/papa31/hm/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
