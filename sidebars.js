9e69afb20e479d25977aeb5efe9427ed4816eacd
papa <visidoor@yandex.ru>
2020-12-05 13:01:09 +0500

    feat: add sidebar with my 2 first dirs
    dirs: workplace & location
    
    	modified:   sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,5 +2,23 @@ module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
+    Workplace: [
+      {
+        type: 'category',
+        label: 'Git',
+        items: [
+          'workplace/git/git-workflow',
+          'workplace/git/conventional-commits',
+          'workplace/git/git-best-practicies'
+        ]
+      },
+    ],
+    Location: [
+      {
+        'Lvovskaya-2020-2021': [
+          'location/Lvovskaya-2020-2021/black-friday-orders/when-starting-black-friday',
+        ]
+      }
+    ]
   },
 };
