58be383961dc54099e9d6df8edfd95986067b4da
PaPa31 <visidoor@yandex.ru>
2022-05-12 16:38:57 +0200

    remove redundant code;
    shortened npm2yarn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 const path = require('path');
 
-// Used to debug production build issues faster
-const isBuildFast = !!process.env.BUILD_FAST;
-
+const npm2yarn = require('@docusaurus/remark-plugin-npm2yarn');
 const baseUrl = process.env.BASE_URL || '/';
-
-const isDeployPreview =
-  !!process.env.NETLIFY && process.env.CONTEXT === 'deploy-preview';
-
-const isI18nStaging = process.env.I18N_STAGING === 'true';
-
-const TwitterSvg =
-  '<svg style="fill: #1DA1F2; vertical-align: middle; margin-left: 3px;" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -32,18 +22,6 @@ const config = {
   favicon: 'img/favicon.ico',
   customFields: {
     description: 'papa31 pandora box',
-  },
-  i18n: {
-    defaultLocale: 'en',
-    // eslint-disable-next-line no-nested-ternary
-    locales: isDeployPreview
-      ? // Deploy preview: keep it fast!
-        ['en']
-      : isI18nStaging
-      ? // Staging locales: https://docusaurus-i18n-staging.netlify.app/
-        ['en', 'ru']
-      : // Production locales
-        ['en', 'ru'],
   },
 
   presets: [
@@ -60,7 +38,7 @@ const config = {
           showLastUpdateTime: true,
           // Please change this to your repo.
           editUrl: 'https://github.com/papa31/hm/edit/documentation/',
-          remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
+          remarkPlugins: [npm2yarn, {sync: true}],
         },
         gtag: {
           trackingID: 'G-M8RTQC8274',
@@ -80,10 +58,10 @@ const config = {
           blogSidebarTitle: 'All posts',
           // Please change this to your repo.
           editUrl: 'https://github.com/papa31/hm/edit/documentation/',
-          remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
+          remarkPlugins: [npm2yarn],
         },
         pages: {
-          remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
+          remarkPlugins: [npm2yarn],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -94,7 +72,9 @@ const config = {
       }),
     ],
   ],
+
   themes: ['@docusaurus/theme-live-codeblock'],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -248,7 +228,7 @@ const config = {
         routeBasePath: 'courses',
         sidebarPath: require.resolve('./sidebarsCourses.js'),
         editUrl: 'https://github.com/papa31/hm/edit/documentation/',
-        remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
+        remarkPlugins: [npm2yarn],
         sidebarCollapsible: true,
         numberPrefixParser: false,
         showLastUpdateAuthor: true,
@@ -265,7 +245,7 @@ const config = {
         include: ['**/*.md'],
         sidebarPath: require.resolve('./sidebarsSupport.js'),
         editUrl: 'https://github.com/papa31/hm/edit/documentation/',
-        remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
+        remarkPlugins: [npm2yarn],
         sidebarCollapsible: true,
         numberPrefixParser: false,
         showLastUpdateAuthor: true,
